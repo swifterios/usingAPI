382571af6c7d91b68799ab079d10be5ecf9111ef
Vladislav <lordroga2@gmail.com>
2021-07-15 07:48:05 +0300

    comments screen

---
+++ usingAPI/usingAPI/Core/Comments/CommentsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CommentsViewController.swift
+//  usingAPI
+//
+//  Created by Владислав on 14.07.2021.
+//
+
+import UIKit
+
+class CommentsViewController: ViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var commentsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        commentsTableView.dataSource = self
+        commentsTableView.delegate = self
+    }
+    
+    
+    //MARK: - TableView functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = commentsTableView.dequeueReusableCell(withIdentifier: "CommentsTableViewCell", for: indexPath) as! CommentsTableViewCell
+        
+        return cell
+    }
+    
+}
